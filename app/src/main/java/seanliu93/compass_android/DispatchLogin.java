a76db39728c096679c9db136477e197a2c64f0dc
seanliu93 <seanliu@bu.edu>
2015-04-13 11:44:56 -0500

    commit

---
+++ app/src/main/java/seanliu93/compass_android/DispatchLogin.java
@@ -0,0 +1,17 @@
+package seanliu93.compass_android;
+
+/**
+ * Created by seanliu93 on 4/1/2015.
+ */
+
+import com.parse.ParseObject;
+import com.parse.ui.ParseLoginDispatchActivity;
+
+public class DispatchLogin extends ParseLoginDispatchActivity {
+
+    @Override
+    protected Class<?> getTargetClass() {
+
+        return MapsActivity.class;
+    }
+}
